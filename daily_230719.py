ab61b6e88c8a8d8c33738d2ca1ed0f0ea0d9ed7e
LWJ <mm9326@naver.com>
2023-07-21 08:43:58 +0900

    last

--- daily_230719.py
+++ daily_230719.py
@@ -90,7 +90,51 @@
  
 #1683 ws_3_4
 
-# number_of_people = 0
+number_of_people = 0
+
+def increase_user():
+    global number_of_people
+    number_of_people += 1
+    return bool(1>0)
+
+name = ['김시습', '허균', '남영로', '임제', '박지원']
+age = [20, 16, 52, 36, 60]
+address = ['서울', '강릉', '조선', '나주', '한성부']
+
+def create_user(*args):
+    user_info = {}
+    user_info['name'] = args[0]
+    user_info['age'] = args[1]
+    user_info['city'] = args[2]
+    return user_info
+
+for name_greeting in name:
+    print(name_greeting + '님 환영합니다!')
+
+
+result = list(map(create_user, name, age, address))
+print (result)
+
+
+# info0 = create_user(name[0], age[0], address[0])
+# info1 = create_user(name[1], age[1], address[1])
+# info2 = create_user(name[2], age[2], address[2])
+# info3 = create_user(name[3], age[3], address[3])
+# info4 = create_user(name[4], age[4], address[4])
+
+# #딕셔너리 묶음
+
+# info = [info0, info1, info2, info3, info4]
+# print(info)
+
+
+# list_info = map(list,info)
+
+# print(list_info)
+
+
+
+# 1684 ws_3_5
 
 # def increase_user():
 #     global number_of_people
@@ -121,42 +165,3 @@
 
 # info = [info0, info1, info2, info3, info4]
 # print(info)
-
-
-# list_info = map(list,info)
-
-# print(list_info)
-
-
-
-# 1684 ws_3_5
-
-def increase_user():
-    global number_of_people
-    number_of_people += 1
-    return bool(1>0)
-
-name = ['김시습', '허균', '남영로', '임제', '박지원']
-age = [20, 16, 52, 36, 60]
-address = ['서울', '강릉', '조선', '나주', '한성부']
-
-def create_user(*args):
-    user_info = {}
-    user_info['name'] = args[0]
-    user_info['age'] = args[1]
-    user_info['city'] = args[2]
-    return user_info
-
-for name_greeting in name:
-    print(name_greeting + '님 환영합니다!')
-
-info0 = create_user(name[0], age[0], address[0])
-info1 = create_user(name[1], age[1], address[1])
-info2 = create_user(name[2], age[2], address[2])
-info3 = create_user(name[3], age[3], address[3])
-info4 = create_user(name[4], age[4], address[4])
-
-#딕셔너리 묶음
-
-info = [info0, info1, info2, info3, info4]
-print(info)
